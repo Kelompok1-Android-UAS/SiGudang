b3e25a497fbe856a32737b37f4a656434351e023
yusup209 <yusup.muhammad209@gmail.com>
2018-10-11 20:40:06 +0700

    Commit pertama

---
+++ app/src/main/java/muhammadyusup/com/sigudang/MainActivity.java
@@ -0,0 +1,15 @@
+package muhammadyusup.com.sigudang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"SiGUdang Ver.1", Toast.LENGTH_LONG);
+    }
+}
